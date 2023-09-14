e98af14464985daa86edc1633570b1892dfbddc3
jihad-A-G <jihadabdlghani73@gmail.com>
2023-09-14 16:27:38 +0300

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,32 +1,86 @@
-import React, { Component } from "react";
-import Search from "./components/Search";
+import React from "react";
+import './App.css'
+import Img1 from '../src/img/weather-icons/cloudy.svg';
+import Img2 from '../src/img/weather-icons/clear.svg';
+import Img3 from '../src/img/weather-icons/snow.svg';
+import Img4 from '../src/img/weather-icons/drizzle.svg';
+import partlyCloudy from '../src/img/weather-icons/partlycloudy.svg';
+import json from './fakeWeatherData.json'
 
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
+console.log(json);
+const App=()=>{
+  return(<>
+ <div className="app">
+  <header>
+ <form>
 
-import "./App.css";
+<label for="country">London</label>
+<button type="submit">Find Weather</button>
+  </form>
+  </header>
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "Karim"
-    };
-  }
+        <div className="images"> 
+       <img id="img1" src={Img1} alt="cloudy-img" />
+       <h1>cloudy</h1>
+       <h2><b>Temperature</b>    10° to 11°C</h2>
+       <p><b>Humidity</b> 78%  <b>Pressure</b>  1008.48</p>
+       </div>
+       <div className="forecast">
 
-  handleInputChange = value => {
-    this.setState({ name: value });
-  };
+        <div className="card">
 
-  render() {
-    return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+          <p>03:00</p>
+          <img src={partlyCloudy} alt="cloudy"/>
+          <p>8° C</p>
+
+        </div>
+        <div className="card">
+
+          <p>03:00</p>
+          <img src={partlyCloudy} alt="cloudy"/>
+          <p>8° C</p>
+
+        </div>
+        <div className="card">
+
+          <p>03:00</p>
+          <img src={Img2} alt="cloudy"/>
+          <p>8° C</p>
+
+        </div>
+        <div className="card">
+
+          <p>03:00</p>
+          <img src={Img2} alt="cloudy"/>
+          <p>8° C</p>
+
+        </div>
+        <div className="card">
+
+          <p>03:00</p>
+          <img src={Img2} alt="cloudy"/>
+          <p>8° C</p>
+
+        </div>
+        <div className="card">
+
+          <p>03:00</p>
+          <img src={Img2} alt="cloudy"/>
+          <p>8° C</p>
+
+        </div>
+        <div className="card">
+
+          <p>03:00</p>
+          <img src={partlyCloudy} alt="cloudy"/>
+          <p>8° C</p>
+
+        </div>
+
+
+       </div>
       </div>
-    );
-  }
+  </>);
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -9,23 +9,24 @@ class Search extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.state.input}
-        <input
-          type="text"
-          id="input-name"
-          onChange={event => {
-            this.setState({ input: event.target.value });
-          }}
-        />
-        <button
-          onClick={event => {
-            this.props.handleInput(this.state.input);
-          }}
-        >
-          Say Hello
-        </button>
-      </div>
+      // <div>
+      //   {this.state.input}
+      //   <input
+      //     type="text"
+      //     id="input-name"
+      //     onChange={event => {
+      //       this.setState({ input: event.target.value });
+      //     }}
+      //   />
+      //   <button
+      //     onClick={event => {
+      //       this.props.handleInput(this.state.input);
+      //     }}
+      //   >
+      //     Say Hello
+      //   </button>
+      // </div>
+      <></>
     );
   }
 }
